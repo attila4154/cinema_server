a8f5ef1a4793e77b6d26d6aff68203eebaee5df7
Danila <attila415422@gmail.com>
2025-02-23 21:10:27 +0100

    db config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,21 @@
+import { neon } from "@neondatabase/serverless";
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { drizzle as drizzlePg } from "drizzle-orm/vercel-postgres";
 
 // todo: implement connection pool instead
-// You can specify any property from the node-postgres connection options
-export const db = drizzle({
-  connection: {
-    connectionString: process.env.DATABASE_URL!,
-    ssl: true,
-  },
-});
+
+let db;
+if (process.env.CONNECT_TO_PROD_DB === "true") {
+  const sql = neon(process.env.DATABASE_URL!);
+  db = drizzleNeon({ client: sql });
+} else {
+  db = drizzlePg({
+    connection: {
+      connectionString: process.env.DATABASE_URL!,
+      ssl: true,
+    },
+  });
+}
+
+export { db };
