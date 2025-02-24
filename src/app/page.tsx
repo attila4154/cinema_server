fee361e00498cb91185f6705674a60ff3499ea28
Danila <attila415422@gmail.com>
2025-02-24 22:09:32 +0100

    trying to use Promise.all in the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 import { parseScreenings } from "@/ext/csfd";
 import { HomePageClient } from "./components/HomePageClient";
-import { getAuthState } from "./service/authorizationService";
+import {
+  AuthState,
+  getAuthState,
+} from "./service/authorizationService";
 import { getCinemasForUser } from "./service/db/cinemaService";
 
-// todo: promise all!!!
+async function getCinemas(authState: AuthState) {
+  if (!authState.loggedIn) return [];
+  return await getCinemasForUser(authState.user.id);
+}
+
 export default async function Home() {
-  const allScreenings = await parseScreenings();
+  const authState = await getAuthState();
+  const [allScreenings, userCinemaIds] = await Promise.all([
+    await parseScreenings(),
+    await getCinemas(authState),
+  ]);
+
   const allCinemas = allScreenings.map((s) => ({
     cinemaId: s.cinemaId,
     cinemaName: s.cinemaName,
   }));
-  const authState = await getAuthState();
-  const userCinemaIds = authState.loggedIn
-    ? await getCinemasForUser(authState.user.id)
-    : [];
 
   return (
     <HomePageClient
