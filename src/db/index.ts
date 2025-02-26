8fb9b60fcf338f3ff85fd96e41925f5cf3b84b77
attila4154 <84931600+attila4154@users.noreply.github.com>
2025-02-26 21:19:28 +0100

    Cache screening data in memory (#10)

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,14 +3,9 @@ import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 
 // todo: implement connection pool instead
 
-// let db: NeonHttpDatabase; //|NodePgDatabase;
 export const db = drizzlePg({
   connection: {
     connectionString: process.env.DATABASE_URL!,
     ssl: false,
   },
 });
-// ? (() => {
-//     const sql = neon(process.env.DATABASE_URL!);
-//     return drizzleNeon({ client: sql });
-//   })()
